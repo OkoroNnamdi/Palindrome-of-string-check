92b4743d226cb8a8bcfab3f60afa58fbe871b054
Okoro Nnamdi <okoronnamdi4044@gmail.com>
2023-01-22 09:09:01 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string to check");
+
+            var name = Console.ReadLine();
+            char[] nameArray = name.ToCharArray();
+            Array.Reverse(nameArray);
+           string reversed = new string (nameArray );
+            if (name .Equals(reversed, StringComparison.OrdinalIgnoreCase ))
+            {
+                Console.WriteLine($"The {name } is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"The {name} is not a palindrome");
+            }
+        }
+    }
+}
